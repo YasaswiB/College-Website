531eaf9603540b8dfc77c55f431e4f55da14211d
YasaswiB <109541072+YasaswiB@users.noreply.github.com>
2023-12-16 11:13:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,37 +14,3 @@ function displayImages() {
   setTimeout(displayImages, 9000); 
 }
 
-// Function to display courses
-function displayCourses() {
-  var coursesList = document.getElementById('courses-list');
-
-  // Clear existing list
-  coursesList.innerHTML = '';
-
-  // Iterate through courses and create list items
-  courses.forEach(function(course) {
-      var listItem = document.createElement('li');
-      listItem.className = 'course-item';
-
-      // Course image
-      var courseImage = document.createElement('img');
-      courseImage.className = 'course-image';
-      courseImage.src = course.image;
-      courseImage.alt = course.name;
-      listItem.appendChild(courseImage);
-
-      // Course details
-      var courseDetails = document.createElement('div');
-      var courseCode = document.createElement('p');
-      courseCode.textContent = 'Code: ' + course.code;
-      var courseName = document.createElement('p');
-      courseName.textContent = 'Name: ' + course.name;
-      courseDetails.appendChild(courseCode);
-      courseDetails.appendChild(courseName);
-
-      listItem.appendChild(courseDetails);
-
-      coursesList.appendChild(listItem);
-  });
-}
-displayCourses();
